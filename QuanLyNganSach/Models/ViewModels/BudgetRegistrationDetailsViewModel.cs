071f7f1ed767bf2efea531c05be37a4dc378265b
HPDQ\nguyenvotuannhan <nguyenvotuannhan@hoaphat.com.vn>
2026-01-13 15:31:59 +0700

    add detail model

---
+++ QuanLyNganSach/Models/ViewModels/BudgetRegistrationDetailsViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuanLyNganSach.Models.ViewModels
+{
+    public class BudgetRegistrationDetailsViewModel
+    {
+        public int BudgetRegistrationId { get; set; }
+        public string TenPhongBan { get; set; }
+        public string MaHangMuc { get; set; }
+        public string TenHangMuc { get; set; }
+        public decimal DuToan { get; set; }
+        public string SoToTrinh { get; set; }
+        public int SoLuong { get; set; }
+        public string LyDoDauTu { get; set; }
+        public string MoTaKyThuat { get; set; }
+        public string LinkTaiLieuLienQuan { get; set; }
+        public int CategoryTypeId { get; set; }
+        public string CategoryTypeName { get; set; }
+        public int PriorityLevelId { get; set; }
+        public string PriorityLevelName { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+        public DateTime NgayTao { get; set; }
+
+        // Attachments
+        public List<BudgetAttachmentViewModel> Attachments { get; set; }
+
+        // Constructor
+        public BudgetRegistrationDetailsViewModel()
+        {
+            Attachments = new List<BudgetAttachmentViewModel>();
+        }
+    }
+
+    /// <summary>
+    /// ViewModel cho file đính kèm
+    /// </summary>
+    public class BudgetAttachmentViewModel
+    {
+        public int AttachmentId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string FileExtension { get; set; }
+        public long FileSize { get; set; }
+        public string FileSizeFormatted { get; set; }
+        public string UploadedBy { get; set; }
+        public DateTime UploadedDate { get; set; }
+    }
+}
---
+++ QuanLyNganSach/Models/ViewModels/BudgetRegistrationListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace QuanLyNganSach.Models.ViewModels
+{
+    public class BudgetRegistrationListViewModel
+    {
+        public int BudgetRegistrationId { get; set; }
+        public int PhongBanId { get; set; }
+        public string MaHangMuc { get; set; }
+        public string TenHangMuc { get; set; }
+        public decimal DuToan { get; set; }
+        public string SoToTrinh { get; set; }
+        public string LyDoDauTu { get; set; }
+        public string MoTaKyThuat { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+        public string TenPhongBan { get; set; }
+        public string NguoiDangKy { get; set; }
+        public string TrangThai { get; set; }
+        public DateTime NgayTao { get; set; }
+    }
+}
